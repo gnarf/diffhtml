c626edfba16e34367a072d63094b2f705fa1a202
Tim Branyen <tim@tabdeveloper.com>
2016-02-20 17:25:06 -0800

    Minor changes

--- docs-es5.js
+++ docs-es5.js
@@ -1,11 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
-
-var _toConsumableArray = function (arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } };
-
-var editor = document.querySelector(".editor");
-var json = document.querySelector(".json");
-var output = document.querySelector("output");
+var editor = document.querySelector('.editor');
+var json = document.querySelector('.json');
+var output = document.querySelector('output');
 
 diff.enableProllyfill();
 
@@ -17,12 +13,23 @@ function text(node) {
     }
 
     return memo.concat(text(node));
-  }, []).join("");
+  }, []).join('');
 }
 
 var defaults = {
-  editorText: "<img\n  width=\"144px\"\n  src=\"kiwi.png\"\n  style=\"\n  \ttransform:\n    \ttranslate({{translate}})\n        rotate({{rotate}})\n  \"\n/>",
-  jsonText: "{\n  \"translate\": [\"0px\", 0],\n  \"rotate\": \"185deg\"\n}"
+  editorText: `<img
+  width="144px"
+  src="kiwi.png"
+  style="
+  	transform:
+    	translate({{translate}})
+        rotate({{rotate}})
+  "
+/>`,
+  jsonText: `{
+  "translate": ["0px", 0],
+  "rotate": "185deg"
+}`
 };
 
 var isRunning = false;
@@ -34,38 +41,39 @@ var offset = output.offsetWidth;
 
 function runAnimation() {
   if (!isRunning) {
-    (function () {
-      var animate = function () {
-        x += increment;
-        rotate += increment;
-        rotate = rotate % 360;
+    rotate = 0;
+    isRunning = true;
+    offset = output.offsetWidth;
+    increment = (x ? -10 : 10) * 1;
 
-        update(null, "{\n  \"translate\": \"" + x + "px, 0\",\n  \"rotate\": \"" + rotate + "deg\"\n}");
+    function animate() {
+      x += increment;
+      rotate += increment;
+      rotate = rotate % 360;
 
-        if (x > offset - 200) {
-          isRunning = false;
-          x = offset - 200;
-        } else if (x <= 0) {
-          isRunning = false;
-          x = 0;
-        } else {
-          requestAnimationFrame(animate);
-        }
-      };
+      update(null, `{
+  "translate": "${ x }px, 0",
+  "rotate": "${ rotate }deg"
+}`);
 
-      rotate = 0;
-      isRunning = true;
-      offset = output.offsetWidth;
-      increment = (x ? -10 : 10) * 1;
+      if (x > offset - 200) {
+        isRunning = false;
+        x = offset - 200;
+      } else if (x <= 0) {
+        isRunning = false;
+        x = 0;
+      } else {
+        requestAnimationFrame(animate);
+      }
+    }
 
-      animate();
-    })();
+    animate();
   }
 }
 
 function update(editorText, jsonText) {
-  editorText = typeof editorText === "string" ? editorText : defaults.editorText;
-  jsonText = typeof jsonText === "string" ? jsonText : defaults.jsonText;
+  editorText = typeof editorText === 'string' ? editorText : defaults.editorText;
+  jsonText = typeof jsonText === 'string' ? jsonText : defaults.jsonText;
 
   defaults.editorText = editorText;
   defaults.jsonText = jsonText;
@@ -77,16 +85,18 @@ function update(editorText, jsonText) {
   diff.innerHTML(output, markup);
   //editor.classList.remove('error');
 
-  if (jsonMirror) {}
+  if (jsonMirror) {
+    //jsonMirror.setValue(jsonText);
+  }
 }
 
 update();
 
-var editorMirror = makeCodeMirror(editor, "editorText");
-var jsonMirror = makeCodeMirror(json, "jsonText");
+var editorMirror = makeCodeMirror(editor, 'editorText');
+var jsonMirror = makeCodeMirror(json, 'jsonText');
 
-jsonMirror.on("change", function (ev) {
-  var editorText = typeof editorText === "string" ? editorText : defaults.editorText;
+jsonMirror.on('change', function (ev) {
+  var editorText = typeof editorText === 'string' ? editorText : defaults.editorText;
   var jsonText = jsonMirror.getValue();
   var data = JSON.parse(jsonText);
   var markup = Mustache.render(editorText, data);
@@ -95,10 +105,10 @@ jsonMirror.on("change", function (ev) {
 });
 
 // Whenever the editor changes, re-render.
-editorMirror.on("change", function (ev) {
+editorMirror.on('change', function (ev) {
   var contents = editorMirror.getValue();
 
-  if (editor.classList.contains("editor")) {
+  if (editor.classList.contains('editor')) {
     update(contents);
   } else {
     update(null, contents);
@@ -109,9 +119,9 @@ function makeCodeMirror(el, name) {
   return CodeMirror(el, { lineWrapping: true, value: defaults[name] });
 }
 
-document.querySelector(".run-animation").onclick = runAnimation;
+document.querySelector('.run-animation').onclick = runAnimation;
 
-var fav = document.querySelector("favorite-movies-chart");
+var fav = document.querySelector('favorite-movies-chart');
 
 function renderFavoriteMoviesLoop() {
   if (fav.render) {
@@ -123,33 +133,29 @@ function renderFavoriteMoviesLoop() {
 
 renderFavoriteMoviesLoop();
 
-document.querySelector(".re-render").onclick = function () {
+document.querySelector('.re-render').onclick = function () {
   fav.makeData();
 };
 
-document.querySelector("aside h1").onclick = function (ev) {
+document.querySelector('aside h1').onclick = function (ev) {
   ev.preventDefault();
   document.body.scrollTop = 0;
 };
 
-document.querySelector(".board").onmouseover = function (ev) {
-  if (ev.target.nodeName.toLowerCase() === "lite-brite") {
-    ev.target.color = document.querySelector("input[type=color]").value;
+document.querySelector('.board').onmouseover = function (ev) {
+  if (ev.target.nodeName.toLowerCase() === 'lite-brite') {
+    ev.target.color = document.querySelector('input[type=color]').value;
   }
 };
 
-document.querySelector(".board").onmouseenter = function (ev) {
-  if (ev.target.nodeName.toLowerCase() === "lite-brite") {
-    ev.target.color = document.querySelector("input[type=color]").value;
+document.querySelector('.board').onmouseenter = function (ev) {
+  if (ev.target.nodeName.toLowerCase() === 'lite-brite') {
+    ev.target.color = document.querySelector('input[type=color]').value;
   }
 };
 
-[].concat(_toConsumableArray(document.querySelectorAll("h2[id]"))).forEach(function (el) {
-  return el.onclick = function (ev) {
-    location.hash = el.id;
-  };
+[...document.querySelectorAll('h2[id]')].forEach(el => el.onclick = ev => {
+  location.hash = el.id;
 });
-
-//jsonMirror.setValue(jsonText);
 
 },{}]},{},[1]);
